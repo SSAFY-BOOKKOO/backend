7af979db4e4a44450fec21b022d24eafbef6a65c
김혁 <kimh0716@gmail.com>
2024-07-16 16:44:14 +0900

    Save my local changes

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import Library from './components/Library/LibraryMain';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: 'library',
+        element: <Library />,
+      },
+    ],
   },
 ]);
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,5 +4,17 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // writing-vertical 유틸리티 클래스를 추가하기 위한 플러그인
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.writing-vertical': {
+          writingMode: 'vertical-rl', // 글씨를 세로로 쓰기 위해 writing-mode 설정
+          textOrientation: 'upright', // 글씨가 위로 직립하도록 설정
+        },
+      };
+
+      addUtilities(newUtilities);
+    },
+  ],
 };
