b967958bc88f0d1ded0b91aa0a03b83e6a98a64f
yunva17 <yunva17@naver.com>
2024-07-17 13:00:42 +0900

    design : wrapContainer padding 수정

--- frontend/src/components/layout/WrapContainer.jsx
+++ frontend/src/components/layout/WrapContainer.jsx
@@ -1,6 +1,6 @@
 const WrapContainer = ({ children }) => {
   return (
-    <div className='max-w-[50rem] mx-auto px-4 py-6 box-border w-full'>
+    <div className='max-w-[50rem] mx-auto px-4 box-border w-full'>
       {children}
     </div>
   );
