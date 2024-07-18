8db3244c1f0ad5da1af416987ad29fa1cc36049b
김인엽 <inyub98@gmail.com>
2024-07-18 10:40:25 +0900

    feat: 책 엔티티 생성

---
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/entity/Book.java
@@ -0,0 +1,35 @@
+package com.ssafy.bookkoo.bookservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "summary")
+    private String summary;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+}
