17dcc8cc94d01b18415ce1a0e7e3c48abdfd3445
김인엽 <inyub98@gmail.com>
2024-07-18 12:51:19 +0900

    feat: swagger 설정 추가

---
+++ backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.ssafy.bookkoo.curationservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Curation API")
+                .version("1.0")
+                .description("Curation API Documentation"));
+    }
+}
