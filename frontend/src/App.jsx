6534ce73c5b5b4ac362a08ba09feb0c4c4d0628e
yejinsong <icdiyj147@gmail.com>
2024-07-18 13:23:57 +0900

    feat: 하단 tab layout 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <Main>
       <Header/>
         <Outlet/>
-      <Footer/>
+
     </Main>
   );
 };
--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,12 +1,46 @@
-// Footer.jsx
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Header = ({ }) => {
+import community from '../../assets/community.png';
+import curation from '../../assets/curation.png';
+import library from '../../assets/library.png';
+import myPage from '../../assets/mypage.png';
+
+const Footer = () => {
+  const navigate = useNavigate();
+
+  const navigateTo = (path) => {
+    navigate(path);
+  };
+
   return (
-    <div className="flex items-center pb-2.5 bg-white">
-      {/* <h1 className="ml-4 text-xl font-medium">{title}</h1> */}
+    <div className="flex justify-around items-center py-2.5 bg-white border-t">
+      <img
+        src={library}
+        alt='library'
+        className="cursor-pointer w-8 h-8"
+        onClick={() => navigateTo('/')}
+      />
+      <img
+        src={community}
+        alt='community'
+        className="cursor-pointer w-8 h-8"
+        onClick={() => navigateTo('/community')}
+      />
+      <img
+        src={curation}
+        alt='curation'
+        className="cursor-pointer w-8 h-8"
+        onClick={() => navigateTo('/curation')}
+      />
+      <img
+        src={myPage}
+        alt='mypage'
+        className="cursor-pointer w-8 h-8"
+        onClick={() => navigateTo('/mypage')}
+      />
     </div>
   );
 };
 
-export default Header;
+export default Footer;
