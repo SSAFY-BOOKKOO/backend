436dd2a864f3e1801cc0d08c86f4cf0aa5c3ecd0
jongho <whdghthdms44@gmail.com>
2024-07-18 11:07:57 +0900

    feat: curation send Entity 정의

---
+++ backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/entity/CurationSend.java
@@ -0,0 +1,31 @@
+package com.ssafy.bookkoo.curationservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class CurationSend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "curation_id")
+    private Curation curation;
+
+    @Column(nullable = false)
+    private Boolean isRead;
+
+    @Column(nullable = false)
+    private Boolean isStored;
+}
