0cd74319502ca47965e42bf344977c0d830fd925
jongho <whdghthdms44@gmail.com>
2024-07-18 11:09:10 +0900

    comment: curation service TODO list

--- backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/service/CurationService.java
+++ backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/service/CurationService.java
@@ -2,4 +2,24 @@ package com.ssafy.bookkoo.curationservice.service;
 
 public interface CurationService {
 
+    //TODO 큐레이션 생성
+
+    //TODO 큐레이션 전송
+
+    //TODO 내가 받은 큐레이션 가져오기
+
+    //TODO 큐레이션 디테일 가져오기 (읽기 처리)
+
+    //TODO 큐레이션 보관하기
+
+    //TODO 큐레이션 삭제 (지정삭제)
+
+    //TODO 큐레이션 스케쥴 삭제
+
+    //TODO 챗봇
+
+    //TODO 알림 메일 전송
+
+    //TODO 내가 보낸 큐레이션 정보 보기
+
 }
