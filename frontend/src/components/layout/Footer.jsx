3663fc5dafa5cdfabaee9dccf4aca0d81af95e30
yunva17 <yunva17@naver.com>
2024-07-19 10:13:12 +0900

    styles : fonts 추가 & 폴더 명 변경

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import community from '../../assets/community.png';
-import curation from '../../assets/curation.png';
-import library from '../../assets/library.png';
-import myPage from '../../assets/mypage.png';
+import community from '@assets/icons/community.png';
+import curation from '@assets/icons/curation.png';
+import library from '@assets/icons/library.png';
+import myPage from '@assets/icons/mypage.png';
 
 const Footer = () => {
   const navigate = useNavigate();
 
-  const navigateTo = (path) => {
+  const navigateTo = path => {
     navigate(path);
   };
 
   return (
-    <div className="flex justify-around items-center py-2.5 bg-white border-t">
+    <div className='flex justify-around items-center py-2.5 bg-white border-t'>
       <img
         src={library}
         alt='library'
-        className="cursor-pointer w-8 h-8"
+        className='cursor-pointer w-8 h-8'
         onClick={() => navigateTo('/')}
       />
       <img
         src={community}
         alt='community'
-        className="cursor-pointer w-8 h-8"
+        className='cursor-pointer w-8 h-8'
         onClick={() => navigateTo('/community')}
       />
       <img
         src={curation}
         alt='curation'
-        className="cursor-pointer w-8 h-8"
+        className='cursor-pointer w-8 h-8'
         onClick={() => navigateTo('/curation')}
       />
       <img
         src={myPage}
         alt='mypage'
-        className="cursor-pointer w-8 h-8"
+        className='cursor-pointer w-8 h-8'
         onClick={() => navigateTo('/mypage')}
       />
     </div>
