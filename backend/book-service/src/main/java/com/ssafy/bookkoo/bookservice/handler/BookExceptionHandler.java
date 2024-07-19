5d1f33e121d04f0473b95ab08efc7e71cde6889c
김인엽 <inyub98@gmail.com>
2024-07-19 14:23:20 +0900

    setting: Book 예외처리 핸들러 추가 Todo : 에러 처리 더 확실히 할 것

--- backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/handler/BookExceptionHandler.java
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/handler/BookExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.ssafy.bookkoo.bookservice.handler;
 
 import com.ssafy.bookkoo.bookservice.exception.BookNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +20,18 @@ public class BookExceptionHandler {
     public ResponseEntity<String> bookNotFoundException(BookNotFoundException e) {
         return ResponseEntity.status(404)
                              .body(e.getMessage());
+    }
+
+    /**
+     * enum 타입에 잘못된 값을 넣으면 나오는 에러
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = "Invalid params value.";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body("{\"error\":\"" + errorMessage + "\"}");
     }
 }
