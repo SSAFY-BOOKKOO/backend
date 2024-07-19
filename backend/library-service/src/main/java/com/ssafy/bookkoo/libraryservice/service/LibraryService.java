6023cafa694a6066ec65e55686b2b9b58b51d90f
김인엽 <inyub98@gmail.com>
2024-07-19 17:45:03 +0900

    feat: 서재 비즈니스 로직 작성

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryService.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryService.java
@@ -1,5 +1,23 @@
 package com.ssafy.bookkoo.libraryservice.service;
 
+import com.ssafy.bookkoo.libraryservice.dto.RequestCreateLibraryDto;
+import com.ssafy.bookkoo.libraryservice.dto.RequestUpdateLibraryDto;
+import com.ssafy.bookkoo.libraryservice.dto.ResponseLibraryDto;
+import java.util.List;
+
 public interface LibraryService {
 
+    ResponseLibraryDto addLibrary(RequestCreateLibraryDto library);
+
+    List<ResponseLibraryDto> getLibraries();
+
+    ResponseLibraryDto getLibrary(Long libraryId);
+
+    ResponseLibraryDto updateLibrary(Long libraryId, RequestUpdateLibraryDto library);
+
+//    ResponseLibraryDto deleteLibrary(Long libraryId);
+
+    Object addBookToLibrary(Long libraryId, Object book);
+
+    Integer countBooksInLibrary(Long memberId);
 }
--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryServiceImpl.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryServiceImpl.java
@@ -1,8 +1,16 @@
 package com.ssafy.bookkoo.libraryservice.service;
 
+import com.ssafy.bookkoo.libraryservice.dto.RequestCreateLibraryDto;
+import com.ssafy.bookkoo.libraryservice.dto.RequestUpdateLibraryDto;
+import com.ssafy.bookkoo.libraryservice.dto.ResponseLibraryDto;
+import com.ssafy.bookkoo.libraryservice.entity.Library;
+import com.ssafy.bookkoo.libraryservice.exception.LibraryNotFoundException;
+import com.ssafy.bookkoo.libraryservice.mapper.LibraryMapper;
 import com.ssafy.bookkoo.libraryservice.repository.LibraryBookMapperRepository;
 import com.ssafy.bookkoo.libraryservice.repository.LibraryRepository;
 import com.ssafy.bookkoo.libraryservice.repository.LibraryStyleRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +21,57 @@ public class LibraryServiceImpl implements LibraryService {
     private final LibraryRepository libraryRepository;
     private final LibraryStyleRepository libraryStyleRepository;
     private final LibraryBookMapperRepository libraryBookMapperRepository;
-    
+
+    private final LibraryMapper libraryMapper = LibraryMapper.INSTANCE;
+
+    @Override
+    @Transactional
+    public ResponseLibraryDto addLibrary(RequestCreateLibraryDto libraryDto) {
+        Library library = libraryMapper.toEntity(libraryDto);
+
+        Library savedLibrary = libraryRepository.save(library);
+
+        return libraryMapper.toResponseDto(savedLibrary);
+    }
+
+    @Override
+    @Transactional
+    public List<ResponseLibraryDto> getLibraries() {
+        List<Library> libraries = libraryRepository.findAll();
+        return libraryMapper.toResponseDtoList(libraries);
+    }
+
+    @Override
+    @Transactional
+    public ResponseLibraryDto getLibrary(Long libraryId) {
+        Library library = findLibraryByIdWithException(libraryId);
+        return libraryMapper.toResponseDto(library);
+    }
+
+    @Override
+    @Transactional
+    public ResponseLibraryDto updateLibrary(Long libraryId, RequestUpdateLibraryDto libraryDto) {
+        Library libraryToUpdate = findLibraryByIdWithException(libraryId);
+        
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Object addBookToLibrary(Long libraryId, Object book) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Integer countBooksInLibrary(Long memberId) {
+        return 0;
+    }
+
+
+    private Library findLibraryByIdWithException(Long libraryId) {
+        return libraryRepository.findById(libraryId)
+                                .orElseThrow(() -> new LibraryNotFoundException(
+                                    "Library not found with id : " + libraryId));
+    }
 }
