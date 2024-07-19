c99e2c17c263fc1845608b5035edecbf860cfe58
jongho <whdghthdms44@gmail.com>
2024-07-19 16:39:51 +0900

    chore: curation open-feign 의존성 추가

---
+++ backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/config/FeignConfig.java
@@ -0,0 +1,10 @@
+package com.ssafy.bookkoo.curationservice.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class FeignConfig {
+
+}
