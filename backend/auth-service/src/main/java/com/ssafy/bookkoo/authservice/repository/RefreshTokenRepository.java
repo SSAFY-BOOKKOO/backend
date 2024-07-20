71b087781cccc8e1ff2c332b24183d0ed81d2d9e
왕승철 <ys4512558@naver.com>
2024-07-21 02:07:31 +0900

    feat: RefreshTokenRepository 개발
    - MemberId를 통해 RefreshToken 객체 찾기 쿼리 개발

---
+++ backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.bookkoo.authservice.repository;
+
+import com.ssafy.bookkoo.authservice.entity.RefreshToken;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByMemberId(String memberId);
+
+}
