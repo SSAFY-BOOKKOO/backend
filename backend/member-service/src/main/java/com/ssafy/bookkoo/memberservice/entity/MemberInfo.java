59ac707f0ecac6cd2fe45a985bb4aa0438ee3bee
ys4512558 <ys4512558@naver.com>
2024-07-22 17:18:07 +0900

    feat: 성별 및 프로필 이미지 URL 필드 추가

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/entity/MemberInfo.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/entity/MemberInfo.java
@@ -3,6 +3,8 @@ package com.ssafy.bookkoo.memberservice.entity;
 import com.ssafy.bookkoo.memberservice.global.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +12,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -24,27 +27,39 @@ public class MemberInfo extends BaseEntity {
     @Column(name = "member_id")
     private String memberId;
 
+    @Setter
     @Column(name = "nick_name")
     private String nickName;
 
     @Column(name = "year")
     private Integer year;
 
+    @Column(name = "gender")
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
     //TODO: 카테고리 목록 후에 추가
     //@Column(name = "categories")
     //private Long categories;
 
+    @Setter
     @Column(name = "introduction")
     private String introduction;
 
+    @Setter
+    @Column(name = "profile_img_url")
+    private String profileImgUrl;
+
     @Builder
-    public MemberInfo(Long id, String memberId, String nickName, Integer year, String categories,
-        String introduction) {
+    public MemberInfo(Long id, String memberId, String nickName, Integer year, Gender gender,
+        String introduction, String profileImgUrl) {
         this.id = id;
         this.memberId = memberId;
         this.nickName = nickName;
         this.year = year;
-//        this.categories = categories;
+        this.gender = gender;
         this.introduction = introduction;
+        this.profileImgUrl = profileImgUrl;
     }
+
 }
