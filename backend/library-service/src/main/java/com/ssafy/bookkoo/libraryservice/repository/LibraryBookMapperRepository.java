6218dddb42e504922a9b2838b2c33fc00f774e96
김인엽 <inyub98@gmail.com>
2024-07-22 23:35:38 +0900

    feat: 서재 책 매퍼 QueryDSL을 사용한 커스텀 레퍼지토리 사용

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/repository/LibraryBookMapperRepository.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/repository/LibraryBookMapperRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.bookkoo.libraryservice.entity.LibraryBookMapper;
 import com.ssafy.bookkoo.libraryservice.entity.MapperKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LibraryBookMapperRepository extends JpaRepository<LibraryBookMapper, MapperKey> {
+public interface LibraryBookMapperRepository extends JpaRepository<LibraryBookMapper, MapperKey>,
+    LibraryBookMapperCustomRepository {
 
 }
