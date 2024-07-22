0cae704c3ab7c0bd9f780bc8206dec6f4195bc83
김인엽 <inyub98@gmail.com>
2024-07-22 23:36:26 +0900

    feat: 서재 서비스 비즈니스 로직 구성 중 - 서재 CRUD + 책 개수 Todo : 서재에 책 등록하기, 서비스 TEST 코드

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/controller/LibraryController.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/controller/LibraryController.java
@@ -1,5 +1,6 @@
 package com.ssafy.bookkoo.libraryservice.controller;
 
+import com.ssafy.bookkoo.libraryservice.dto.RequestBookDto;
 import com.ssafy.bookkoo.libraryservice.dto.RequestCreateLibraryDto;
 import com.ssafy.bookkoo.libraryservice.dto.RequestUpdateLibraryDto;
 import com.ssafy.bookkoo.libraryservice.dto.ResponseLibraryDto;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,20 +28,23 @@ public class LibraryController {
     @PostMapping
     @Operation(summary = "서재 생성", description = "서재 생성 API")
     public ResponseEntity<ResponseLibraryDto> createLibrary(@RequestBody RequestCreateLibraryDto dto) {
-        return null;
+        return ResponseEntity.ok()
+                             .body(libraryService.addLibrary(dto));
     }
 
     @GetMapping
-    @Operation(summary = "서재 목록 조회", description = "서재 목록 조회 API")
-    public ResponseEntity<List<ResponseLibraryDto>> getLibraries() {
-        return null;
+    @Operation(summary = "서재 목록 조회", description = "해당 사용자의 서재 목록 조회 API")
+    // 임시로 memberId 받는걸로
+    public ResponseEntity<List<ResponseLibraryDto>> getLibraries(@RequestParam Long memberId) {
+        return ResponseEntity.ok()
+                             .body(libraryService.getLibrariesOfMember(memberId));
     }
 
     @GetMapping("/{libraryId}")
     @Operation(summary = "서재 단일 조회", description = "서재 단일 조회 API")
     public ResponseEntity<ResponseLibraryDto> getLibrary(@PathVariable Long libraryId) {
-
-        return null;
+        return ResponseEntity.ok()
+                             .body(libraryService.getLibrary(libraryId));
     }
 
 //    @GetMapping("/{libraryId}/books/{bookId}")
@@ -54,24 +59,28 @@ public class LibraryController {
         @PathVariable Long libraryId,
         @RequestBody RequestUpdateLibraryDto libraryDto
     ) {
-        return null;
+        return ResponseEntity.ok()
+                             .body(libraryService.updateLibrary(libraryId, libraryDto));
     }
 
     @PostMapping("/{libraryId}/books")
     @Operation(summary = "서재에 책 등록", description = "서재에 책 등록하는 API")
     public ResponseEntity<Object> addBookToLibrary(
         @PathVariable Long libraryId,
-        @RequestBody Object book
+        @RequestBody RequestBookDto book
     ) {
         // 1. 해당 책이 DB에 있는지 확인 후 없으면 생성하는 api 요청하기
         // 2. 해당 책에 대한 id 값을 받아서 책-서재 매퍼에 추가해 책 등록하기
-        return null;
+        return ResponseEntity.ok()
+                             .body(libraryService.addBookToLibrary(libraryId, book));
     }
 
     @GetMapping("/books/count")
     @Operation(summary = "사용자가 등록한 모든 책 개수", description = "사용자가 본인 서재에 등록한 모든 책 개수")
-    public ResponseEntity<Integer> countBooks() {
+    // 임시로 memberId 받는걸로
+    public ResponseEntity<Integer> countBooks(@RequestParam Long memberId) {
         // 그런것들
-        return null;
+        return ResponseEntity.ok()
+                             .body(libraryService.countBooksInLibrary(memberId));
     }
 }
