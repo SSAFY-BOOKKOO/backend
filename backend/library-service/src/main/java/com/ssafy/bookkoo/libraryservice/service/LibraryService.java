aa10d460ea27ae28ed415927f67b6fc3ff8382b2
김인엽 <inyub98@gmail.com>
2024-07-22 23:37:14 +0900

    feat: Object -> 책 DTO로 변경

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryService.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryService.java
@@ -1,5 +1,6 @@
 package com.ssafy.bookkoo.libraryservice.service;
 
+import com.ssafy.bookkoo.libraryservice.dto.RequestBookDto;
 import com.ssafy.bookkoo.libraryservice.dto.RequestCreateLibraryDto;
 import com.ssafy.bookkoo.libraryservice.dto.RequestUpdateLibraryDto;
 import com.ssafy.bookkoo.libraryservice.dto.ResponseLibraryDto;
@@ -9,15 +10,21 @@ public interface LibraryService {
 
     ResponseLibraryDto addLibrary(RequestCreateLibraryDto library);
 
-    List<ResponseLibraryDto> getLibraries();
+    List<ResponseLibraryDto> getLibrariesOfMember(Long memberId);
 
     ResponseLibraryDto getLibrary(Long libraryId);
 
-    ResponseLibraryDto updateLibrary(Long libraryId, RequestUpdateLibraryDto library);
+    ResponseLibraryDto updateLibrary(
+        Long libraryId,
+        RequestUpdateLibraryDto library
+    );
 
 //    ResponseLibraryDto deleteLibrary(Long libraryId);
 
-    Object addBookToLibrary(Long libraryId, Object book);
+    Object addBookToLibrary(
+        Long libraryId,
+        RequestBookDto book
+    );
 
     Integer countBooksInLibrary(Long memberId);
 }
