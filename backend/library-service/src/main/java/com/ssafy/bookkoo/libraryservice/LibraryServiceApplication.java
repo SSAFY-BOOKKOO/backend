b5a14ba6344c5714b5b5ff12b78299d403e485f9
김인엽 <inyub98@gmail.com>
2024-07-23 12:54:11 +0900

    feat: 서비스 간 통신을 위한 openfeign dependency 추가 및 설정 추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/LibraryServiceApplication.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/LibraryServiceApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.bookkoo.libraryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LibraryServiceApplication {
 
     public static void main(String[] args) {
