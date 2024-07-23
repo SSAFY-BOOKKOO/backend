14285fd145a0b90a232f0d79b80a68c97006d4e8
김인엽 <inyub98@gmail.com>
2024-07-23 16:23:13 +0900

    feat: books setter추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/dto/ResponseLibraryDto.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/dto/ResponseLibraryDto.java
@@ -1,11 +1,13 @@
 package com.ssafy.bookkoo.libraryservice.dto;
 
 import java.util.List;
+import lombok.Setter;
 
 public record ResponseLibraryDto(
     String name,
     String libraryOrder,
     LibraryStyleDto libraryStyleDto,
+    @Setter
     List<ResponseBookDto> books
 ) {
 
