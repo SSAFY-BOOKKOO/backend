bc690ce9bc7062d949a21a32ecde10ac0e87dfa4
jongho <whdghthdms44@gmail.com>
2024-07-23 13:38:52 +0900

    fix: curation URL prefix 수정

--- backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/controller/CurationController.java
+++ backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/controller/CurationController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/curation")
+@RequestMapping("/curations")
 @RequiredArgsConstructor
 public class CurationController {
 
