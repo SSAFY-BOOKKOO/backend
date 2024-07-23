4fb0f0daf23f26c7c5b9b5d4a829a814de125d27
김인엽 <inyub98@gmail.com>
2024-07-23 12:54:40 +0900

    feat: openfeign 스타일로 클라이언트 변경

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/client/BookServiceClient.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/client/BookServiceClient.java
@@ -2,33 +2,26 @@ package com.ssafy.bookkoo.libraryservice.client;
 
 import com.ssafy.bookkoo.libraryservice.dto.RequestBookDto;
 import com.ssafy.bookkoo.libraryservice.dto.ResponseBookDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import com.ssafy.bookkoo.libraryservice.dto.ResponseCheckBooksByIsbnDto;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Component
-@RequiredArgsConstructor
-public class BookServiceClient {
+@FeignClient(name = "book-service", url = "http://127.0.0.1:8002/books")
+public interface BookServiceClient {
 
-    private final WebClient.Builder webClientBuilder;
-    private final String BASE_URL = "http://127.0.0.1:8000/books";
+    @PostMapping
+    ResponseBookDto addBook(@RequestBody RequestBookDto requestBookDto);
 
-    public ResponseBookDto addBook(RequestBookDto bookDto) {
-        return webClientBuilder.build()
-                               .post()
-                               .uri(BASE_URL)
-                               .bodyValue(bookDto)
-                               .retrieve()
-                               .bodyToMono(ResponseBookDto.class)
-                               .block();
-    }
+    @GetMapping("/{bookId}")
+    ResponseBookDto getBookById(@PathVariable("bookId") String bookId);
 
-    public ResponseBookDto getBook(Long bookId) {
-        return webClientBuilder.build()
-                               .get()
-                               .uri(BASE_URL + "/{bookId}", bookId)
-                               .retrieve()
-                               .bodyToMono(ResponseBookDto.class)
-                               .block();
-    }
+    @GetMapping("/isbn/{isbn}")
+    ResponseBookDto getBookByIsbn(@PathVariable("isbn") String isbn);
+
+    @PostMapping("/check-books")
+    List<ResponseCheckBooksByIsbnDto> checkBooksByIsbn(@RequestBody String[] isbnList);
 }
