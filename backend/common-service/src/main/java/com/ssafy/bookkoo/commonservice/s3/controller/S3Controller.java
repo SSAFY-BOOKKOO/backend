cb9dae2d1f435629beedcd3d43ae3623dd73c377
ys4512558 <ys4512558@naver.com>
2024-07-23 17:48:21 +0900

    feat: member의 pk를 헤더에 잘 받아오는지 테스트하기 위해 로깅 추가

--- backend/common-service/src/main/java/com/ssafy/bookkoo/commonservice/s3/controller/S3Controller.java
+++ backend/common-service/src/main/java/com/ssafy/bookkoo/commonservice/s3/controller/S3Controller.java
@@ -2,14 +2,18 @@ package com.ssafy.bookkoo.commonservice.s3.controller;
 
 import com.ssafy.bookkoo.commonservice.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/commons/file")
@@ -19,9 +23,14 @@ public class S3Controller {
 
     @PostMapping
     public ResponseEntity<HttpStatus> uploadFile(
+        @RequestHeader HttpHeaders headers,
         @RequestPart("file") MultipartFile file,
         @RequestPart(value = "bucket", required = false) String bucket
     ) {
+        for (String s : headers.keySet()) {
+            log.info("headers[{}] : {}", s, headers.get(s));
+
+        }
         s3Service.saveToBucket(file, bucket);
         return ResponseEntity.ok()
                              .build();
