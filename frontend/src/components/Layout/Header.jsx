757e4be9a5c3474dc439be546dadbca2b54dca8a
yunva17 <yunva17@naver.com>
2024-07-23 13:20:08 +0900

    design : 헤더로 가려지는 오류 디자인 수정

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -3,7 +3,7 @@ import BackButton from '../@common/Back';
 
 const Header = ({}) => {
   return (
-    <header className='flex items-center p-3 w-full'>
+    <header className='flex items-center p-3 w-full '>
       <BackButton />
       {/* <h1 className="ml-4 text-xl font-medium">{title}</h1> */}
     </header>
--- frontend/src/components/Layout/WrapContainer.jsx
+++ frontend/src/components/Layout/WrapContainer.jsx
@@ -1,6 +1,6 @@
 const WrapContainer = ({ children, className }) => {
   return (
-    <div className={`w-full px-4 box-border ${className}`}>{children}</div>
+    <div className={`w-full px-4 py-1 box-border ${className}`}>{children}</div>
   );
 };
 
