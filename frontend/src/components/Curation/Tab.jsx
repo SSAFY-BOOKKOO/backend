d58e4bc55124c7eaff072be7f9788fecf19cac42
yunva17 <yunva17@naver.com>
2024-07-23 13:19:03 +0900

    feat : 챗봇 플로팅 버튼

--- frontend/src/components/Curation/Tab.jsx
+++ frontend/src/components/Curation/Tab.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import letterPlus from '../../assets/icons/letterPlus.png';
-
+import ChatbotFloatingButton from './ChatbotFloatingButton';
 
 const Tab = () => {
   return (
-    <div className="flex justify-between items-center p-4 border-b-2 border-black">
-      <div className="flex space-x-4 text-lg">
+    <div className='flex justify-between items-center p-4 border-b-2 border-black'>
+      <div className='flex space-x-4 text-lg'>
         <NavLink
-          to="/curation/receive"
+          to='/curation/receive'
           className={({ isActive }) =>
             `px-4 py-2 ${isActive ? 'font-bold text-black' : 'text-gray-500'}`
           }
@@ -17,7 +17,7 @@ const Tab = () => {
           받은 레터
         </NavLink>
         <NavLink
-          to="/curation/send"
+          to='/curation/send'
           className={({ isActive }) =>
             `px-4 py-2 ${isActive ? 'font-bold text-black' : 'text-gray-500'}`
           }
@@ -25,7 +25,7 @@ const Tab = () => {
           보낸 레터
         </NavLink>
         <NavLink
-          to="/curation/store"
+          to='/curation/store'
           className={({ isActive }) =>
             `px-4 py-2 ${isActive ? 'font-bold text-black' : 'text-gray-500'}`
           }
@@ -33,12 +33,10 @@ const Tab = () => {
           보관 레터
         </NavLink>
       </div>
-      <NavLink
-        to="/curation/letter-create"
-        className="px-4 py-2 rounded"
-      >
-        <img src={letterPlus} alt="Create Letter" className="w-6 h-6" />
+      <NavLink to='/curation/letter-create' className='px-4 py-2 rounded'>
+        <img src={letterPlus} alt='Create Letter' className='w-6 h-6' />
       </NavLink>
+      <ChatbotFloatingButton />
     </div>
   );
 };
