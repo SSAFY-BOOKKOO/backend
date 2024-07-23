d356b1a5ffcc2b57647329eca117626fc6e789f3
왕승철 <ys4512558@naver.com>
2024-07-23 22:53:31 +0900

    refactor: 토큰 인증 필터 구조 개선 및 커스텀 예외 throw구문 추가

--- backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/filter/TokenAuthenticationFilter.java
+++ backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/filter/TokenAuthenticationFilter.java
@@ -1,8 +1,10 @@
 package com.ssafy.bookkoo.bookkoogateway.config.filter;
 
 import com.ssafy.bookkoo.bookkoogateway.client.MemberServiceWebClient;
+import com.ssafy.bookkoo.bookkoogateway.exception.TokenExpirationException;
 import com.ssafy.bookkoo.bookkoogateway.util.TokenUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.Authentication;
@@ -14,6 +16,7 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter implements WebFilter {
@@ -25,37 +28,37 @@ public class TokenAuthenticationFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        log.info("토큰 인증 필터 진입");
         //헤더에서 Authorization 뽑기
         String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
         //Authorization에서 Bearer를 통해 액세스 토큰 뽑기
         String accessToken = getAccessToken(authorizationHeader);
 
-        if (accessToken != null && tokenUtils.validToken(accessToken)) {
-            Authentication authentication = tokenUtils.getAuthentication(accessToken);
-            String subject = tokenUtils.getSubject(accessToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            System.out.println("Request received with valid token, subject: " + subject);
-
-            return memberServiceWebClient.getMemberId(subject)
-                                      .flatMap(id -> {
-                                          ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
-                                                                                     .header(PASSPORT_HEADER, String.valueOf(id))
-                                                                                     .build();
-                                          ServerWebExchange mutatedExchange = exchange.mutate().request(mutatedRequest).build();
-
-                                          System.out.println("Received member ID: " + id);
-
-                                          return chain.filter(mutatedExchange)
-                                                      .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
-                                      })
-                                      .doOnError(error -> {
-                                          System.err.println("Error occurred: " + error.getMessage());
-                                      });
-        } else {
-            System.out.println("토큰 엄서");
-            return chain.filter(exchange);
+        if(accessToken == null || !tokenUtils.validToken(accessToken)) {
+            log.info("토큰이 만료되었습니다.");
+            throw new TokenExpirationException();
         }
+
+        Authentication authentication = tokenUtils.getAuthentication(accessToken);
+        String subject = tokenUtils.getSubject(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return memberServiceWebClient.getMemberId(subject)
+                                     .flatMap(id -> {
+                                         //Passpord 정보를 담은 헤더를 세팅
+                                         ServerHttpRequest mutatedRequest = exchange.getRequest()
+                                                                                    .mutate()
+                                                                                    .header(PASSPORT_HEADER, String.valueOf(id))
+                                                                                    .build();
+                                         //헤더 정보를 담은 Request를 추가한 exchange를 다음 필터로 전달
+                                         return chain.filter(exchange.mutate()
+                                                                     .request(mutatedRequest)
+                                                                     .build())
+                                                     .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
+                                     })
+                                     .doOnError(error -> {
+                                         log.error("토큰 필터 에러 {}", error.getMessage());
+                                     });
     }
 
     private String getAccessToken(String authorizationHeader) {
