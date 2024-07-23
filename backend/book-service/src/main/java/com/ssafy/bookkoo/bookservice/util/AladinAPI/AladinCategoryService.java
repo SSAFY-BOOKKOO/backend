d71e4c37f93298797cc91d8a7394a96aa131f007
김인엽 <inyub98@gmail.com>
2024-07-24 00:40:39 +0900

    feat: 알라딘 카테고리 매핑을 위한 서비스 로직 추가

---
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/util/AladinAPI/AladinCategoryService.java
@@ -0,0 +1,38 @@
+package com.ssafy.bookkoo.bookservice.util.AladinAPI;
+
+import com.ssafy.bookkoo.bookservice.entity.AladinCategoryMapper;
+import com.ssafy.bookkoo.bookservice.entity.Category;
+import com.ssafy.bookkoo.bookservice.repository.AladinCategoryMapperRepository;
+import com.ssafy.bookkoo.bookservice.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AladinCategoryService {
+
+    final private AladinCategoryMapperRepository categoryMapperRepository;
+    final private CategoryRepository categoryRepository;
+
+    public Integer convertToServiceCategoryId(Integer aladinCategoryId) {
+
+        AladinCategoryMapper mapper = categoryMapperRepository.findByAladinCategoryId(
+            aladinCategoryId);
+        if (mapper != null) {
+            return mapper.getCategory()
+                         .getId();
+        } else {
+            // If no mapping found, return ID for "기타" category or null
+            Category otherCategory = categoryRepository.findByName("기타");
+            return otherCategory != null ? otherCategory.getId() : null;
+        }
+    }
+
+    public void processApiResponse(ResponseAladinAPI response) {
+        for (AladinBookItem item : response.getItem()) {
+            Integer aladinCategoryId = item.getCategoryId();
+            Integer serviceCategoryId = convertToServiceCategoryId(aladinCategoryId);
+            item.setCategoryId(serviceCategoryId);
+        }
+    }
+}
