c1f34e4ef9de65d7192d58fad5b8217e299eaa1d
yejinsong <icdiyj147@gmail.com>
2024-07-25 17:12:16 +0900

    fix: 큐레이션 메인 피드백 수정

--- frontend/src/pages/Library/LibraryDetail.jsx
+++ frontend/src/pages/Library/LibraryDetail.jsx
@@ -46,20 +46,22 @@ const LibraryDetail = () => {
             />
           ) : (
             // 책 큰 틀
+
             <div className='relative  bg-zinc-300 rounded-lg w-10/12 max-w-md h-full overflow-auto '>
               <Modal bookId={id} onDelete={handleDelete} />
               <div className='flex flex-col items-center p-8 pl-12'>
-                <div className='absolute left-6 top-0 bottom-0 shadow-2xl w-1 bg-gra y-500 shadow-2xl z-10'></div>
                 <img
                   src={cover_img_url}
                   alt={title}
-                  className='w-72 h-96 cursor-pointer rounded-lg shadow-xl'
+                  className='w-72 h-96 cursor-pointer rounded-lg shadow-xl mr-4'
                   onClick={() => setShowReview(true)}
                 />
                 {/* <button>전환</button> */}
               </div>
+              <div className='absolute left-4 top-0 bottom-0 shadow-2xl w-1 bg-gray-500 shadow-2xl z-20'></div>
+
               {/* 띠지 부분 */}
-              <div className='mt-6 p-4 pl-14 bg-pink-500 rounded-b-md opacity-70 w-full  h-[215px]'>
+              <div className='mt-6 p-4 pl-14 bg-pink-500 rounded-b-md opacity-70 w-full  h-[216px]'>
                 <h2 className=' text-2xl text-black font-bold'>{title}</h2>
                 <div className='flex space-x-1'>
                   {Array(5).fill(<AiFillStar className='text-amber-300' />)}
