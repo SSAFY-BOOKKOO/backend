32a9e7b23cb6b0d9fe2dfeffee38546f6382d028
ys4512558 <ys4512558@naver.com>
2024-07-25 12:26:59 +0900

    feat: 멤버 정보에 MapStruct 변경
    - 카테고리 ID를 반환하도록 변경

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/mapper/MemberInfoMapper.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/mapper/MemberInfoMapper.java
@@ -1,15 +1,23 @@
 package com.ssafy.bookkoo.memberservice.mapper;
 
 import com.ssafy.bookkoo.memberservice.dto.response.ResponseMemberInfoDto;
+import com.ssafy.bookkoo.memberservice.entity.MemberCategoryMapper;
 import com.ssafy.bookkoo.memberservice.entity.MemberInfo;
+import java.util.List;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface MemberInfoMapper {
 
-    MemberInfoMapper INSTANCE = Mappers.getMapper(MemberInfoMapper.class);
 
     // 엔티티를 DTO로 변환
+    @Mapping(source = "categories", target = "categories", qualifiedByName = "mapCategories")
     ResponseMemberInfoDto toResponseDto(MemberInfo memberInfo);
+
+    @Named("mapCategories")
+    default List<Integer> mapCategories(List<MemberCategoryMapper> categories) {
+        return categories.stream().map(category -> category.getMemberCategoryMapperKey().getCategoryId()).toList();
+    }
 }
