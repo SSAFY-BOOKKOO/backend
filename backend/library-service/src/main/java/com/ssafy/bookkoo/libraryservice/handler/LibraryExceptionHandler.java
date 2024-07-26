4964ec83481f2c64d279224371a375f8b0585a1c
김인엽 <inyub98@gmail.com>
2024-07-26 15:19:59 +0900

    feat: 해당 닉네임에 대한 Member가 없을 때에 대한 커스텀 에러 처리 추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/handler/LibraryExceptionHandler.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/handler/LibraryExceptionHandler.java
@@ -2,6 +2,8 @@ package com.ssafy.bookkoo.libraryservice.handler;
 
 import com.ssafy.bookkoo.libraryservice.exception.BookAlreadyMappedException;
 import com.ssafy.bookkoo.libraryservice.exception.LibraryNotFoundException;
+import com.ssafy.bookkoo.libraryservice.exception.MemberNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,6 +26,12 @@ public class LibraryExceptionHandler {
     @ExceptionHandler(BookAlreadyMappedException.class)
     public ResponseEntity<String> illegalStateException(BookAlreadyMappedException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body(e.getMessage());
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<String> memberNotFoundException(MemberNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
                              .body(e.getMessage());
     }
 }
