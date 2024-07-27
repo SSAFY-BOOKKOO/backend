f22b8eb06abfebf4fdd0efadb2f603a9be82328a
김인엽 <inyub98@gmail.com>
2024-07-27 10:39:55 +0900

    feat: 서재 조회 로직에 filter(Status) 파라미터 추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryService.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/service/LibraryService.java
@@ -4,15 +4,22 @@ import com.ssafy.bookkoo.libraryservice.dto.RequestCreateLibraryDto;
 import com.ssafy.bookkoo.libraryservice.dto.RequestLibraryBookMapperCreateDto;
 import com.ssafy.bookkoo.libraryservice.dto.RequestUpdateLibraryDto;
 import com.ssafy.bookkoo.libraryservice.dto.ResponseLibraryDto;
+import com.ssafy.bookkoo.libraryservice.entity.Status;
 import java.util.List;
 
 public interface LibraryService {
 
-    ResponseLibraryDto addLibrary(RequestCreateLibraryDto library, Long memberId);
+    ResponseLibraryDto addLibrary(
+        RequestCreateLibraryDto library,
+        Long memberId
+    );
 
     List<ResponseLibraryDto> getLibrariesOfMember(String nickname);
 
-    ResponseLibraryDto getLibrary(Long libraryId, String filter);
+    ResponseLibraryDto getLibrary(
+        Long libraryId,
+        Status filter
+    );
 
     ResponseLibraryDto updateLibrary(
         Long libraryId,
