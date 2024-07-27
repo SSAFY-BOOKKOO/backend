a763add969c99be68f88b64fe2167370403f0901
김인엽 <inyub98@gmail.com>
2024-07-27 10:40:53 +0900

    feat: Member Service에서 nickname으로 memberId 가져오는 로직 추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/client/MemberServiceClient.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/client/MemberServiceClient.java
@@ -1,5 +1,6 @@
 package com.ssafy.bookkoo.libraryservice.client;
 
+import com.ssafy.bookkoo.libraryservice.exception.MemberNotFoundException;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,13 @@ public interface MemberServiceClient {
 
     String prefix = "/members";
 
-    @GetMapping(prefix + "/name")
-    Long getMemberIdByNickName(@RequestParam("nickname") String nickname);
+    /**
+     * nickname으로 memberId 받아오는 메서드 : 해당 사람의 서재 목록 찾을 때 사용됨
+     *
+     * @param nickname : nickname
+     * @return memberId
+     * @throws MemberNotFoundException(404)
+     */
+    @GetMapping(prefix + "/info/name")
+    Long getMemberIdByNickName(@RequestParam("nickName") String nickname);
 }
