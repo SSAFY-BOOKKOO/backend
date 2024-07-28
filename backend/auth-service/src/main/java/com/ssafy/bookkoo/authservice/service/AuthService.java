876b90da8e31ffde781dafd06589a9362ed0bede
왕승철 <ys4512558@naver.com>
2024-07-29 00:53:42 +0900

    feat: 메서드 추가

--- backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/service/AuthService.java
+++ backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/service/AuthService.java
@@ -2,10 +2,16 @@ package com.ssafy.bookkoo.authservice.service;
 
 import com.ssafy.bookkoo.authservice.dto.RequestLoginDto;
 import com.ssafy.bookkoo.authservice.dto.ResponseLoginTokenDto;
+import com.ssafy.bookkoo.authservice.entity.Member;
+import java.util.Optional;
 
 public interface AuthService {
 
     ResponseLoginTokenDto login(RequestLoginDto requestLoginDto);
 
     ResponseLoginTokenDto getTokenDto(String refreshToken);
+
+    Optional<Member> getMemberByEmail(String email);
+
+    ResponseLoginTokenDto getResponseLoginTokenDto(Member member);
 }
