55442fe759189290eee636f1e1f5d4fc922266b7
왕승철 <ys4512558@naver.com>
2024-07-28 19:37:50 +0900

    feat: Swagger에 Authorize필드 추가

--- backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/SwaggerConfig.java
+++ backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.ssafy.bookkoo.bookkoogateway.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(servers = {
-    @Server(url = "/", description = "Default Server URL")
+@OpenAPIDefinition(
+    servers = {@Server(url = "/", description = "Default Server URL")
 })
 @Configuration
 public class SwaggerConfig {
