52defb5849bd70d426df2a5afc0defe0592e4b1a
왕승철 <ys4512558@naver.com>
2024-07-29 00:55:46 +0900

    feat: auth-service 필터 설정 추가

--- backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/config/SecurityConfig.java
@@ -1,19 +1,61 @@
 package com.ssafy.bookkoo.authservice.config;
 
+import com.ssafy.bookkoo.authservice.handler.OAuth2SuccessHandler;
+import com.ssafy.bookkoo.authservice.repository.OAuth2AuthorizationRequestBasedOnRepository;
+import com.ssafy.bookkoo.authservice.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    /**
-     * 패스워드 해시값 매칭을 위한 인코더 빈 등록
-     * @return
-     */
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2AuthorizationRequestBasedOnRepository oAuth2AuthorizationRequestBasedOnRepository;
+    private final CustomOAuth2UserService customOAuth2UserService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers(
+                    "/auth-service/**",
+                    "/api-docs/**",
+                    "/auth/login/**",
+                    "/auth/token",
+                    "/css/**",
+                    "/images/**",
+                    "/js/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html",
+                    "/docs/**",
+                    "/webjars/**",
+                    "/v3/api-docs/**",
+                    "/swagger-resources/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint
+                    .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnRepository))
+                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                    .userService(customOAuth2UserService))
+                .successHandler(oAuth2SuccessHandler))
+            .build();
     }
 }
