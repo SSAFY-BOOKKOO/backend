64904d24b7476742fadf9a132e548cf616c6d602
왕승철 <ys4512558@naver.com>
2024-07-29 22:13:02 +0900

    comment: 주석 추가

--- backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/service/CustomOAuth2MemberService.java
+++ backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/service/CustomOAuth2MemberService.java
@@ -7,7 +7,6 @@ import com.ssafy.bookkoo.authservice.dto.OAuth.OAuth2UserInfo;
 import com.ssafy.bookkoo.authservice.entity.Member;
 import com.ssafy.bookkoo.authservice.exception.MemberNotFoundException;
 import com.ssafy.bookkoo.authservice.repository.MemberRepository;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -25,13 +24,21 @@ public class CustomOAuth2MemberService extends DefaultOAuth2UserService {
     private final MemberServiceClient memberServiceClient;
     private final MemberRepository memberRepository;
 
+    /**
+     * 요청을 통해 받은 UserRequest를 통해 유저 정보를 받아오는 메서드
+     * @param userRequest
+     * @return
+     * @throws OAuth2AuthenticationException
+     */
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
+        //제공한 권한 서버의 ID (google, kakao, naver)
         String registrationId = userRequest.getClientRegistration()
                                            .getRegistrationId();
         OAuth2UserInfo oAuth2UserInfo = null;
+        //Provider들 각각이 key를 다르게 가지고 있음 필요한 것을 추상 클래스를 통해 공통으로 꺼내올 수 있도록 처리
         switch (registrationId) {
             case "google":
                 oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
@@ -45,15 +52,13 @@ public class CustomOAuth2MemberService extends DefaultOAuth2UserService {
                 throw new OAuth2AuthenticationException("Provider Not Valid");
         }
 
-        log.info("registrationId : {}", registrationId);
-        log.info("email : {}", Optional.ofNullable(oAuth2User.getAttribute("account_email")));
-
         String email = oAuth2UserInfo.getEmail();
         //없는 유저이면 등록
         if (memberRepository.findByMemberId(email)
                             .isEmpty()) {
             memberServiceClient.register(email);
         }
+
         Member member = memberRepository.findByEmail(email)
                                         .orElseThrow(MemberNotFoundException::new);
 
