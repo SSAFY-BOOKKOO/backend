2f2bf5cc0ae9090d146dd60383f23a0947134e9c
김인엽 <inyub98@gmail.com>
2024-07-29 17:59:59 +0900

    feat: library book mapper update 추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/mapper/LibraryBookMapperMapper.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/mapper/LibraryBookMapperMapper.java
@@ -1,12 +1,29 @@
 package com.ssafy.bookkoo.libraryservice.mapper;
 
 import com.ssafy.bookkoo.libraryservice.dto.RequestLibraryBookMapperCreateDto;
+import com.ssafy.bookkoo.libraryservice.dto.RequestLibraryBookMapperUpdateDto;
 import com.ssafy.bookkoo.libraryservice.entity.LibraryBookMapper;
 import com.ssafy.bookkoo.libraryservice.entity.MapperKey;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface LibraryBookMapperMapper {
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "bookColor", source = "dto.bookColor")
+    @Mapping(target = "bookOrder", source = "dto.bookOrder")
+    @Mapping(target = "startAt", source = "dto.startAt")
+    @Mapping(target = "endAt", source = "dto.endAt")
+    @Mapping(target = "status", source = "dto.status")
+    @Mapping(target = "rating", source = "dto.rating")
+    void updateLibraryBookMapperFromDto(
+        RequestLibraryBookMapperUpdateDto dto,
+        @MappingTarget LibraryBookMapper entity
+    );
 
     default LibraryBookMapper toEntity(RequestLibraryBookMapperCreateDto dto, MapperKey mapperKey) {
         return LibraryBookMapper.builder()
