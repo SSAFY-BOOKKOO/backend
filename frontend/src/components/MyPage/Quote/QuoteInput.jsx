6a1c876efe849d328e3764e9916faf767e27a703
김지윤 <yunva17@naver.com>
2024-07-29 14:14:55 +0900

    design : 글귀 입력 페이지 수정

--- frontend/src/components/MyPage/Quote/QuoteInput.jsx
+++ frontend/src/components/MyPage/Quote/QuoteInput.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Input from '../../@common/Input';
+import Textarea from '../../@common/Textarea';
+import Button from '../../@common/Button';
 
 const QuoteInput = ({ addQuote, setShowInput }) => {
   const [quote, setQuote] = useState('');
@@ -15,36 +18,32 @@ const QuoteInput = ({ addQuote, setShowInput }) => {
   };
 
   return (
-    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-20'>
-      <div className='bg-white p-6 rounded-lg shadow-lg w-full max-w-md'>
-        <h2 className='text-xl font-bold mb-4'>문장 입력</h2>
+    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-20 '>
+      <div className='bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-md'>
+        <h2 className='text-xl font-bold mb-4'>글귀 입력</h2>
         <form onSubmit={handleSubmit}>
-          <textarea
+          <Textarea
             placeholder='여기에 문장을 입력해주세요'
             value={quote}
             onChange={e => setQuote(e.target.value)}
-            className='border p-2 mb-4 w-full h-32'
+            maxLength={200}
           />
-          <input
+          <Input
             type='text'
             placeholder='페이지 및 기타 정보 (예: p.29)'
             value={info}
             onChange={e => setInfo(e.target.value)}
-            className='border p-2 mb-4 w-full'
           />
-          <div className='flex justify-end'>
-            <button
-              type='submit'
-              className='bg-green-500 text-white p-2 rounded-lg mr-2'
-            >
+          <div className='flex justify-end mt-3'>
+            <Button type='submit' className='mr-3'>
               완료
-            </button>
-            <button
+            </Button>
+            <Button
               onClick={() => setShowInput(false)}
-              className='bg-gray-500 text-white p-2 rounded-lg'
+              color='bg-gray-100 text-gray-700 hover:bg-gray-200'
             >
               취소
-            </button>
+            </Button>
           </div>
         </form>
       </div>
