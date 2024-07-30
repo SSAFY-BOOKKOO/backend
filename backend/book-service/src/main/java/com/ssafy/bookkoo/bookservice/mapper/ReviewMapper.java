07b8bb4f962944e7cb426f6a955abfcc586246cb
김인엽 <inyub98@gmail.com>
2024-07-30 17:48:15 +0900

    feat: 리뷰 매퍼 추가

---
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/mapper/ReviewMapper.java
@@ -0,0 +1,20 @@
+package com.ssafy.bookkoo.bookservice.mapper;
+
+import com.ssafy.bookkoo.bookservice.dto.RequestReviewDto;
+import com.ssafy.bookkoo.bookservice.dto.ResponseReviewDto;
+import com.ssafy.bookkoo.bookservice.entity.Review;
+import java.util.List;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
+
+    // DTO -> Entity 변환
+    Review toEntity(RequestReviewDto dto);
+
+    // Entity -> DTO 변환
+    ResponseReviewDto toDto(Review review);
+
+    // Entity -> DTO 리스트 변환
+    List<ResponseReviewDto> toDto(List<Review> reviews);
+}
