47165233351ddd2b6381edc82f0d509eab2630eb
김인엽 <inyub98@gmail.com>
2024-07-30 11:03:19 +0900

    feat: DTO에 빌더 추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/dto/ResponseLibraryBookMapperDto.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/dto/ResponseLibraryBookMapperDto.java
@@ -2,7 +2,9 @@ package com.ssafy.bookkoo.libraryservice.dto;
 
 import com.ssafy.bookkoo.libraryservice.entity.Status;
 import java.sql.Date;
+import lombok.Builder;
 
+@Builder
 public record ResponseLibraryBookMapperDto(
     ResponseBookDto book,
     Integer bookOrder,
