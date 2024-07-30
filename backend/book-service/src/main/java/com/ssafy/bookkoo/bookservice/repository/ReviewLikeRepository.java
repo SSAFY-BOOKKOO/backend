09430cca018e0b9b8dd7d15d573ea60944271907
김인엽 <inyub98@gmail.com>
2024-07-30 17:48:06 +0900

    feat: 리뷰 좋아요 레포지토리 추가

---
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/repository/ReviewLikeRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.bookkoo.bookservice.repository;
+
+import com.ssafy.bookkoo.bookservice.entity.ReviewLike;
+import com.ssafy.bookkoo.bookservice.entity.ReviewMemberId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewLikeRepository extends JpaRepository<ReviewLike, ReviewMemberId> {
+
+}
