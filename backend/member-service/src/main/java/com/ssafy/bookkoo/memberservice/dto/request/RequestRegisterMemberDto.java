d976fe35d96e505fa8e297b7bc993b00a99ba62e
ys4512558 <ys4512558@naver.com>
2024-07-30 17:45:11 +0900

    feat: 회원가입 시 전체 정보를 한번에 받도록 DTO변경

---
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestRegisterMemberDto.java
@@ -0,0 +1,33 @@
+package com.ssafy.bookkoo.memberservice.dto.request;
+
+import com.ssafy.bookkoo.memberservice.annotation.MaxArray;
+import com.ssafy.bookkoo.memberservice.entity.Gender;
+import com.ssafy.bookkoo.memberservice.entity.SocialType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+
+@Builder
+public record RequestRegisterMemberDto (
+    @Pattern(
+        regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
+        message = "Invalid email address"
+    ) String email,
+    String password,
+    @NotNull(message = "nickName can not be null") String nickName,
+    Integer year,
+    Gender gender,
+    @MaxArray(value = 15) Integer[] categories,
+    String introduction,
+    SocialType socialType
+) {
+
+    /**
+     * SocialType이 없으면 bookkoo로 설정
+     */
+    public RequestRegisterMemberDto {
+        if (socialType == null) {
+            socialType = SocialType.bookkoo;
+        }
+    }
+}
