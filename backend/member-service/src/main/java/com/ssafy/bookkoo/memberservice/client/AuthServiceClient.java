259ccb20b071ceb32b2c34783e68cc85f4314a21
왕승철 <ys4512558@naver.com>
2024-07-30 22:44:20 +0900

    comment: 주석 제거

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/client/AuthServiceClient.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/client/AuthServiceClient.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//TODO: WebClient로 바꿔보기
 @FeignClient(name = "auth-service")
 public interface AuthServiceClient {
 
