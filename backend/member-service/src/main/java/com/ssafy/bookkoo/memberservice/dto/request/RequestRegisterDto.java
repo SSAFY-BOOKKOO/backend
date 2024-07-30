bd93e435b6a92d7c61f52a58b488efd7c5e13aba
왕승철 <ys4512558@naver.com>
2024-07-30 22:44:47 +0900

    feat: 비밀번호 유효성 검사 삭제 (검사 로직을 서비스로 위임)

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestRegisterDto.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestRegisterDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.bookkoo.memberservice.dto.request;
 
 import com.ssafy.bookkoo.memberservice.entity.SocialType;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
@@ -14,15 +15,12 @@ import lombok.Builder;
  */
 @Builder
 public record RequestRegisterDto(
+    @NotNull
     @Pattern(
         regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
         message = "Invalid email address"
     ) String email,
-    //null은 가능
-    @Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
-        message = "Invalid password."
-    ) String password,
+    String password,
     SocialType socialType) {
 
 }
