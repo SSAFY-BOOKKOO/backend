b361896c3240e67edeee08235aece3874632d758
김인엽 <inyub98@gmail.com>
2024-07-30 15:41:41 +0900

    feat: id 리스트로 카테고리 조회하는 함수 추가

--- backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/repository/CategoryRepository.java
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/repository/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.bookkoo.bookservice.repository;
 
 import com.ssafy.bookkoo.bookservice.entity.Category;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     Category findByName(String name);
+
+    List<Category> findByIdIn(List<Integer> ids);
 }
