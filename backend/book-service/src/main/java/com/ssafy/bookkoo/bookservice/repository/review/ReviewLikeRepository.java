96317bb5b9fcf04d73f74160700e226d0a7d6796
김인엽 <inyub98@gmail.com>
2024-07-31 14:05:59 +0900

    feat: review 복합키로 찾는 메서드 추가

--- backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/repository/review/ReviewLikeRepository.java
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/repository/review/ReviewLikeRepository.java
@@ -2,8 +2,10 @@ package com.ssafy.bookkoo.bookservice.repository.review;
 
 import com.ssafy.bookkoo.bookservice.entity.ReviewLike;
 import com.ssafy.bookkoo.bookservice.entity.ReviewMemberId;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewLikeRepository extends JpaRepository<ReviewLike, ReviewMemberId> {
 
+    Optional<ReviewLike> findById(ReviewMemberId id);
 }
