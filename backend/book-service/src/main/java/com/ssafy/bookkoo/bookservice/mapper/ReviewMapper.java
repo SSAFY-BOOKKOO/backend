7212a5fa08e5b32032c694a9a872ecc8d1418897
김인엽 <inyub98@gmail.com>
2024-07-31 14:06:19 +0900

    feat: review mapper 좋아요 개수, bookId 매퍼 추가

--- backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/mapper/ReviewMapper.java
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/mapper/ReviewMapper.java
@@ -5,6 +5,7 @@ import com.ssafy.bookkoo.bookservice.dto.review.ResponseReviewDto;
 import com.ssafy.bookkoo.bookservice.entity.Review;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
@@ -13,8 +14,12 @@ public interface ReviewMapper {
     Review toEntity(RequestReviewDto dto);
 
     // Entity -> DTO 변환
+    @Mapping(target = "bookId", source = "book.id")
+    @Mapping(target = "likeCount", expression = "java(review.getLikeCount())")
     ResponseReviewDto toDto(Review review);
 
     // Entity -> DTO 리스트 변환
+    @Mapping(target = "bookId", source = "book.id")
+    @Mapping(target = "likeCount", expression = "java(review.getLikeCount())")
     List<ResponseReviewDto> toDto(List<Review> reviews);
 }
