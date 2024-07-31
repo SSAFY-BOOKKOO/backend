63e695ac06ab976ec8bcea7813412d25dbe29f09
김지윤 <yunva17@naver.com>
2024-07-31 17:02:38 +0900

    fix: 검색결과에 따른 예외 추가(더보기)

--- frontend/src/components/Library/Search/SearchResultSection.jsx
+++ frontend/src/components/Library/Search/SearchResultSection.jsx
@@ -12,32 +12,40 @@ const SearchResultSection = ({
   return (
     <div className='mb-6 flex justify-center flex-col'>
       <h2 className='text-lg font-bold mb-2'>{title}</h2>
-      <div
-        className={`flex mb-2 ${type === 'booktalk' ? 'flex-col' : 'justify-center'}`}
-      >
-        {books
-          .slice(0, 3)
-          .map(book =>
-            type === 'booktalk' ? (
-              <BookTalkItem
-                key={book.book_id}
-                book={book}
-                onClick={() => onItemClick(book)}
-              />
-            ) : (
-              <BookItem
-                key={book.book_id}
-                book={book}
-                onClick={() => onItemClick(book)}
-              />
-            )
-          )}
-      </div>
-      <div className='flex justify-end text-sm cursor-pointer'>
-        <button className='text-gray-500' onClick={onSeeMore}>
-          더보기
-        </button>
-      </div>
+      {books?.length === 0 ? (
+        <div className='flex flex-col items-center bg-gray-100 space-y-4 p-4 m-4 rounded'>
+          <p>검색 결과가 없습니다.</p>
+        </div>
+      ) : (
+        <div
+          className={`flex mb-2 ${type === 'booktalk' ? 'flex-col' : 'justify-center'}`}
+        >
+          {books
+            ?.slice(0, 3)
+            .map(book =>
+              type === 'booktalk' ? (
+                <BookTalkItem
+                  key={book.book_id}
+                  book={book}
+                  onClick={() => onItemClick(book)}
+                />
+              ) : (
+                <BookItem
+                  key={book.book_id}
+                  book={book}
+                  onClick={() => onItemClick(book)}
+                />
+              )
+            )}
+        </div>
+      )}
+      {books?.length > 3 && (
+        <div className='flex justify-end text-sm cursor-pointer'>
+          <button className='text-gray-500' onClick={onSeeMore}>
+            더보기
+          </button>
+        </div>
+      )}
     </div>
   );
 };
