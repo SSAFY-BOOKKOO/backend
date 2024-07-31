8caa405a14ec75c2a86c6bad841db83644346b02
ys4512558 <ys4512558@naver.com>
2024-07-31 17:51:51 +0900

    feat: 회원가입/로그인인 경우를 판단하여 분리
    - 회원가입인 경우 소셜 로그인을 통해 얻은 정보를 클라이언트로 반환
    - 로그인인 경우 토큰 관련 정보를 클라이언트로 반환

--- backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/handler/OAuth2SuccessHandler.java
+++ backend/auth-service/src/main/java/com/ssafy/bookkoo/authservice/handler/OAuth2SuccessHandler.java
@@ -1,9 +1,14 @@
 package com.ssafy.bookkoo.authservice.handler;
 
-import com.ssafy.bookkoo.authservice.client.MemberServiceClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.bookkoo.authservice.dto.ResponseLoginTokenDto;
+import com.ssafy.bookkoo.authservice.dto.ResponseSocialRegisterDto;
+import com.ssafy.bookkoo.authservice.dto.SocialRegisterDto;
 import com.ssafy.bookkoo.authservice.entity.Member;
-import com.ssafy.bookkoo.authservice.entity.SocialType;
+import com.ssafy.bookkoo.authservice.service.AuthService;
+import com.ssafy.bookkoo.authservice.util.CookieUtils;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,8 +16,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -24,7 +27,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
-    private final MemberServiceClient memberServiceClient;
+    private static final String REFRESH_TOKEN_NAME = "refresh_token";
+    private final AuthService authService;
+    private final ObjectMapper objectMapper;
 
     @Value("${config.frontend}")
     private String FRONTEND_URL;
@@ -44,12 +49,48 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         //이메일
         String email = authentication.getName();
 
-        Authentication authentication1 = SecurityContextHolder.getContext()
-                                                              .getAuthentication();
-        //TODO: 형변환되는지 확인해보고 이를 활용하도록 하기
-        //TODO: 가능하다면 ResponseBody를 통해 OAuth2 멤버정보를 클라이언트로 반환하도록하기
-        Member member = (Member) authentication.getPrincipal();
-        response.sendRedirect(FRONTEND_URL + "/library");
+        log.info("authentication : {}", authentication);
+        log.info("authentication principal: {}", authentication.getPrincipal());
+        Object principal = authentication.getPrincipal();
+        // ResponseSocialRegisterDto이면 ROLE_GUEST
+        if (principal instanceof SocialRegisterDto socialRegisterDto) {
+            ResponseSocialRegisterDto registerDto
+                = toResponseSocialRegisterDto(socialRegisterDto);
+            log.info("socialRegisterDto : {}", socialRegisterDto);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            //소셜 로그인 정보를 클라이언트로 전송하고 추가정보를 입력받아 회원가입 완료
+            response.getWriter().write(objectMapper.writeValueAsString(registerDto));
+        } else { //Member이면 ROLE_USER (로그인 처리 토큰 전송)
+            Member member = (Member) principal;
+            log.info("Member : {}", member);
+            ResponseLoginTokenDto tokenDto = authService.login(member.getEmail());
+            Cookie cookie = CookieUtils.secureCookieGenerate(REFRESH_TOKEN_NAME,
+                tokenDto.refreshToken(), CookieUtils.REFRESH_TOKEN_EXPIRATION);
+            response.addCookie(cookie);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            //토큰 관련 정보를 추가하여 로그인 처리
+            response.getWriter().write(objectMapper.writeValueAsString(tokenDto));
+        }
+    }
 
+    /**
+     * 클라이언트로 반환하기 위해 소셜 멤버 정보를 컨버팅
+     * @param socialRegisterDto
+     * @return
+     */
+    public ResponseSocialRegisterDto toResponseSocialRegisterDto(
+        SocialRegisterDto socialRegisterDto) {
+        StringBuilder nickName = new StringBuilder();
+        nickName.append(socialRegisterDto.getSocialType());
+        nickName.append("_");
+        nickName.append(socialRegisterDto.getEmail());
+        return ResponseSocialRegisterDto.builder()
+                                        .email(socialRegisterDto.getEmail())
+                                        .nickName(nickName.toString())
+                                        .socialType(socialRegisterDto.getSocialType())
+                                        .profileImgUrl(socialRegisterDto.getProfileImgUrl())
+                                        .build();
     }
 }
