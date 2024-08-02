4cd111ab3c7ba3190bd65c754bc15ac49d0c7a48
김지윤 <yunva17@naver.com>
2024-08-02 17:44:36 +0900

    feat : library api 추가

--- frontend/src/services/Library.js
+++ frontend/src/services/Library.js
@@ -1,5 +1,6 @@
 import { authAxiosInstance } from './axiosInstance';
 
+// 내 서재 검색
 export const getLibrarySearchBooks = async (
   text,
   tag,
@@ -22,6 +23,31 @@ export const getLibrarySearchBooks = async (
       'libraries/me/books/search',
       bodyData
     );
+    return response.data;
+  } catch (error) {
+    console.error('aladin books failed:', error);
+    throw error;
+  }
+};
+
+// 서재에 책 등록
+export const postLibraryBook = async (libraryId, info) => {
+  try {
+    const response = await authAxiosInstance.post(
+      `libraries/${libraryId}/books`,
+      info
+    );
+    return response.data;
+  } catch (error) {
+    console.error('aladin books failed:', error);
+    throw error;
+  }
+};
+
+// 내가 가진 서재 목록 조회
+const getLibraryList = async () => {
+  try {
+    const response = await authAxiosInstance.post('libraries/me');
     return response.data;
   } catch (error) {
     console.error('aladin books failed:', error);
