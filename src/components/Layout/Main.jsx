4e2de7c76a2e791bdd4f1604ff3f591afe1fe459
yejinsong <icdiyj147@gmail.com>
2024-08-02 12:12:46 +0900

    Revert "fix: 서재 상세페이지 수정 및 아이콘 삭제"
    This reverts commit 8b7c8ac5d56a4ba5f6641e350164f80c9da72999.

--- src/components/Layout/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Main = ({ children }) => (
-  <div className='flex flex-col h-screen bg-white shadow-md max-w-md mx-auto'>
-    {children}
-  </div>
-);
-
-export default Main;
--- src/pages/Mypage/Statistics.jsx
+++
@@ -1,7 +0,0 @@
-import { books } from '@mocks/BookData';
-
-const Statistics = () => {
-  return <p>차트</p>;
-};
-
-export default Statistics;
