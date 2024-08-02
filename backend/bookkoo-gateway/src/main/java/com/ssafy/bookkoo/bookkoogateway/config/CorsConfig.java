a0cb605426892837171330864246b42be78ff375
jongho <whdghthdms44@gmail.com>
2024-08-02 15:18:54 +0900

    fix: infra cors 설정 변경

--- backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/CorsConfig.java
+++ backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.bookkoo.authservice.config;
+package com.ssafy.bookkoo.bookkoogateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
