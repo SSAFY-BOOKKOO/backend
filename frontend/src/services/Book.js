0f378698813cef9e8a6f0ea78780c5f2115dcadf
김지윤 <yunva17@naver.com>
2024-08-02 17:43:57 +0900

    comment : 주석 추가

--- frontend/src/services/Book.js
+++ frontend/src/services/Book.js
@@ -1,5 +1,6 @@
 import { authAxiosInstance } from './axiosInstance';
 
+// 알라딘 전체 검색(title, author, publisher별)
 export const getAladinBooks = async (text, tag, start = 1, maxResult = 4) => {
   try {
     const params = {
@@ -20,6 +21,7 @@ export const getAladinBooks = async (text, tag, start = 1, maxResult = 4) => {
   }
 };
 
+// 알라딘 단일 검색(isbn)
 export const getAladinBookByIsbn = async isbn => {
   try {
     const response = await authAxiosInstance.get(`/books/aladin/books/${isbn}`);
