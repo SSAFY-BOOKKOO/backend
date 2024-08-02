bc94b4feca8486ad8d8bff06c893aec05ef1a6ff
왕승철 <ys4512558@naver.com>
2024-08-03 01:33:15 +0900

    comment: 액세스 토큰 만료 예외에 주석 추가

--- backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/exception/AccessTokenExpirationException.java
+++ backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/exception/AccessTokenExpirationException.java
@@ -1,5 +1,8 @@
 package com.ssafy.bookkoo.bookkoogateway.exception;
 
+/**
+ * 액세스 토큰 만료 시 발생하는 예외
+ */
 public class AccessTokenExpirationException extends RuntimeException {
     public AccessTokenExpirationException() {
         super("액세스 토큰이 만료되었습니다.");
