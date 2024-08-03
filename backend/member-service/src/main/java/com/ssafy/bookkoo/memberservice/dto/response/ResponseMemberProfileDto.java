618973da6ac2d39a2b98baa9d20c15b97a382b2f
왕승철 <ys4512558@naver.com>
2024-08-04 03:56:37 +0900

    feat: 멤버 서비스의 ProfileDto에 memberId (UUID)필드 추가

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/response/ResponseMemberProfileDto.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/response/ResponseMemberProfileDto.java
@@ -1,8 +1,12 @@
 package com.ssafy.bookkoo.memberservice.dto.response;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record ResponseMemberProfileDto(
+    String memberId,
     String email,
     String nickName,
     String profileImgUrl,
