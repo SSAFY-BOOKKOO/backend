9f70d8feb1de7f4157edff70f43d54d7bc06fac4
왕승철 <ys4512558@naver.com>
2024-08-04 03:57:37 +0900

    feat: 알림 관련 리포지토리 개발

---
+++ backend/notification-service/src/main/java/com/ssafy/bookkoo/notificationservice/repository/NotificationCustomRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.ssafy.bookkoo.notificationservice.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.bookkoo.notificationservice.entity.Notification;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import javax.management.NotificationFilter;
+import java.util.List;
+
+import static com.ssafy.bookkoo.notificationservice.entity.QNotification.notification;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class NotificationCustomRepositoryImpl implements NotificationCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Notification> findByMemberIdAndCondition(String memberId, Pageable pageable) {
+        log.info("pageSize = {}", pageable.getPageSize());
+        log.info("offset = {}", pageable.getOffset());
+
+        return queryFactory.selectFrom(notification)
+                           .where(notification.memberId.eq(memberId))
+                           .offset(pageable.getOffset())
+                           .limit(pageable.getPageSize())
+                           .fetch();
+    }
+}
