38f142bc2a31219bfe46b44eddfca96afbf25163
김지윤 <yunva17@naver.com>
2024-08-06 12:20:49 +0900

    feat : 알림 클릭 페이지 이동, 알림 메세지 추가

--- frontend/src/components/Notification/NotificationItem.jsx
+++ frontend/src/components/Notification/NotificationItem.jsx
@@ -1,28 +1,70 @@
 import React from 'react';
 import { FaRegBell } from 'react-icons/fa6';
+import { useNavigate } from 'react-router-dom';
 
 const NotificationItem = ({ notification }) => {
+  const navigate = useNavigate();
+
   const renderNotification = () => {
     switch (notification?.notificationType) {
       case 'follow':
-        return `${notification?.nickName}님이 회원님을 팔로우했습니다.`;
+        return (
+          <>
+            새로운 독서 친구!
+            <br />
+            {notification?.nickName}님이 회원님을 팔로우했어요.
+          </>
+        );
       case 'curation':
-        return `큐레이션 레터가 도착했습니다.`;
+        return (
+          <>
+            당신을 위한 책 선물!
+            <br />
+            {notification?.nickName}님으로부터 큐레이션 레터가 도착했습니다.
+          </>
+        );
       case 'community':
-        return `회원님이 참여한 북톡이 사라질 예정입니다.`;
+        return (
+          <>
+            {notification.title} 북톡방이 곧 사라질 예정입니다.
+            <br />
+            소중한 대화들을 되돌아보세요.
+          </>
+        );
       default:
-        return `알 수 없는 알림입니다.`;
+        return `새로운 소식이 있어요! 지금 확인해보세요.`;
+    }
+  };
+
+  const handlePageMove = e => {
+    e.stopPropagation();
+
+    switch (notification?.notificationType) {
+      case 'follow':
+        navigate(``);
+        return;
+      case 'curation':
+        navigate(`/curation/letter/${notification?.curationId}`);
+        return;
+      case 'community':
+        navigate(`/booktalk/detail/${notification?.communityId}`);
+        return;
+      default:
+        return;
     }
   };
 
   return (
-    <li className='px-4 py-6 flex items-center'>
-      <div className='bg-yellow-400 rounded-full p-2 mr-4'>
+    <li
+      className='px-4 py-6 flex items-center justify-start'
+      onClick={handlePageMove}
+    >
+      <div className='flex items-center justify-center bg-green-400 rounded-full p-3 mr-4'>
         <FaRegBell className='text-white' />
       </div>
       <div className='flex-1'>
-        <p className='text-sm text-gray-600'>{renderNotification()}</p>
-        <p className='text-xs text-gray-400 mt-1'></p>
+        <p className='text-base text-gray-600'>{renderNotification()}</p>
+        <p className='text-xs text-gray-400 mt-1'>{notification?.createdAt}</p>
       </div>
     </li>
   );
