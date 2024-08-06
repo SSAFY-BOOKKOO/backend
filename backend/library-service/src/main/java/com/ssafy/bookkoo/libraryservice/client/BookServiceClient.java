06c2e7cbccb58575e4ae3e2d0ec01694b132cfc2
김인엽 <inyub98@gmail.com>
2024-08-06 17:35:59 +0900

    feat : headers -> memberId로 바꾸기 (다른 사람이 들어올 때 처리를 위함)

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/client/BookServiceClient.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/client/BookServiceClient.java
@@ -7,12 +7,11 @@ import com.ssafy.bookkoo.libraryservice.dto.other.ResponseBookOfLibraryDto;
 import com.ssafy.bookkoo.libraryservice.dto.other.ResponseCheckBooksByIsbnDto;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * BookService와 통신하기 위한 FeignClient 인터페이스입니다.
@@ -80,8 +79,8 @@ public interface BookServiceClient {
 
     @GetMapping(prefix + "/{bookId}/me")
     ResponseBookOfLibraryDto getBookOfLibrary(
-        @RequestHeader HttpHeaders headers,
-        @PathVariable Long bookId
+        @PathVariable Long bookId,
+        @RequestParam Long memberId
     );
 
 }
