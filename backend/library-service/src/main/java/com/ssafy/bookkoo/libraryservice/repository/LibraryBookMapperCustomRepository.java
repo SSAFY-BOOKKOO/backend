62fe95f8e99cee6eea2da7919347036df3f26501
김인엽 <inyub98@gmail.com>
2024-08-07 09:01:36 +0900

    feat : 서재 단일 조회 시 페이지네이션 추가

--- backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/repository/LibraryBookMapperCustomRepository.java
+++ backend/library-service/src/main/java/com/ssafy/bookkoo/libraryservice/repository/LibraryBookMapperCustomRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.bookkoo.libraryservice.repository;
 import com.ssafy.bookkoo.libraryservice.entity.LibraryBookMapper;
 import com.ssafy.bookkoo.libraryservice.entity.Status;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 /**
  * LibraryBookMapper 엔티티에 대한 사용자 정의 쿼리 메서드를 제공하는 인터페이스입니다.
@@ -32,7 +33,11 @@ public interface LibraryBookMapperCustomRepository {
      * @param filter    상태 필터
      * @return 필터링된 LibraryBookMapper 엔티티 리스트
      */
-    List<LibraryBookMapper> findByLibraryIdWithFilter(Long libraryId, Status filter);
+    List<LibraryBookMapper> findByLibraryIdWithFilter(
+        Long libraryId,
+        Status filter,
+        Pageable pageable
+    );
 
     /**
      * 특정 멤버 ID에 대한 책 ID 리스트를 반환합니다.
