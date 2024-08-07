070a18489a24ccb9bfa38b89c0cd79f52ce25893
jongho <whdghthdms44@gmail.com>
2024-08-07 16:09:10 +0900

    fix: BookTalk MongoDB Auditing 적용

---
+++ backend/booktalk-service/src/main/java/com/ssafy/bookkoo/booktalkservice/config/MongoConfig.java
@@ -0,0 +1,11 @@
+package com.ssafy.bookkoo.booktalkservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@EnableMongoAuditing
+@Configuration
+public class MongoConfig {
+
+}
