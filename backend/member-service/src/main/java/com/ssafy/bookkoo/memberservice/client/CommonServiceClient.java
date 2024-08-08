fb2fda06ca6566e1d2e4d49996cc83ba530bcc16
ys4512558 <ys4512558@naver.com>
2024-08-08 11:59:11 +0900

    feat: 멤버 서비스에서 이메일 발송을 위한 서비스호출 메서드 정의

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/client/CommonServiceClient.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/client/CommonServiceClient.java
@@ -1,9 +1,11 @@
 package com.ssafy.bookkoo.memberservice.client;
 
+import com.ssafy.bookkoo.memberservice.client.dto.request.RequestSendEmailDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,4 +22,7 @@ public interface CommonServiceClient {
     void deleteProfileImg(
         @RequestParam(name = "file") String file,
         @RequestParam(name = "bucket") String bucket);
+
+    @PostMapping(value = "/commons/email")
+    void sendMail(@RequestBody RequestSendEmailDto sendEmailDto);
 }
