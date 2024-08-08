3fdfa38406fd7930cb402e95eb8931b69d9cb6a1
jongho <whdghthdms44@gmail.com>
2024-08-08 15:14:47 +0900

    feat: Curation exception 메세지 오타 수정

--- backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/exception/CurationNotFoundException.java
+++ backend/curation-service/src/main/java/com/ssafy/bookkoo/curationservice/exception/CurationNotFoundException.java
@@ -3,7 +3,7 @@ package com.ssafy.bookkoo.curationservice.exception;
 public class CurationNotFoundException extends RuntimeException {
 
     public CurationNotFoundException(Long id) {
-        super(" 큐레이션을 찾을 수가 없어요");
+        super("큐레이션을 찾을 수가 없어요");
     }
 
 }
