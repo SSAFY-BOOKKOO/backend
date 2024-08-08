979fd327a55cb4d3d987553de37ce69f53b2f39d
ys4512558 <ys4512558@naver.com>
2024-08-08 14:27:13 +0900

    feat: 수신자 정보 필드를 채우기 위해 쿼리 추가 개발

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/repository/custom/MemberInfoCustomRepository.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/repository/custom/MemberInfoCustomRepository.java
@@ -1,9 +1,11 @@
 package com.ssafy.bookkoo.memberservice.repository.custom;
 
+import com.ssafy.bookkoo.memberservice.dto.response.ResponseRecipientDto;
 import java.util.List;
 
 public interface MemberInfoCustomRepository {
 
     List<Long> findRandomMemberInfoIdByFollowers(List<Long> followers);
 
+    List<ResponseRecipientDto> findByRecipientsInfoByIds(List<Long> recipientIds);
 }
