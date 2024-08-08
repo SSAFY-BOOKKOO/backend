88f3e7d67f29b5daa38be49de37225fdd59406f2
jongho <whdghthdms44@gmail.com>
2024-08-08 12:29:53 +0900

    fix: BookTalk BookNotFound 에러코드 변경

--- backend/booktalk-service/src/main/java/com/ssafy/bookkoo/booktalkservice/handler/BookExceptionHandler.java
+++ backend/booktalk-service/src/main/java/com/ssafy/bookkoo/booktalkservice/handler/BookExceptionHandler.java
@@ -1,16 +1,17 @@
 package com.ssafy.bookkoo.booktalkservice.handler;
 
 import com.ssafy.bookkoo.booktalkservice.exception.BookNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class BookExceptionHandler {
-    
+
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<String> bookNotFoundException(BookNotFoundException e) {
-        return ResponseEntity.status(404)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
                              .body(e.getMessage());
     }
 
