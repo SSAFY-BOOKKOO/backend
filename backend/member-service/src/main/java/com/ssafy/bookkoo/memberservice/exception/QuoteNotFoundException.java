c3290af14a254f88ed374536259e1ab451050b1f
왕승철 <ys4512558@naver.com>
2024-08-08 22:57:41 +0900

    feat: 글귀 없을 때 예외 메시지 수정

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/exception/QuoteNotFoundException.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/exception/QuoteNotFoundException.java
@@ -6,6 +6,6 @@ package com.ssafy.bookkoo.memberservice.exception;
 public class QuoteNotFoundException extends RuntimeException {
 
     public QuoteNotFoundException() {
-        super("글귀가 존재하지 않을 때 발생하는 예외");
+        super("글귀가 존재하지 않습니다.");
     }
 }
