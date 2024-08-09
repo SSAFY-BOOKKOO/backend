9b5899961c4be4fa48ae98914bb0d2a1a19dceb9
김지윤 <yunva17@naver.com>
2024-08-09 12:05:39 +0900

    feat: 채팅 더보기 목록

---
+++ frontend/src/hooks/useMyBookTalkInfiniteScroll.jsx
@@ -0,0 +1,20 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getMyBookTalk } from '@services/BookTalk';
+
+const useMyBookTalkInfiniteScroll = (order = 'time') => {
+  return useInfiniteQuery({
+    queryKey: ['myBookTalks', order],
+    queryFn: ({ pageParam = 0 }) => getMyBookTalk(order, pageParam),
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length < 10) {
+        return undefined;
+      }
+      return allPages.length * 10;
+    },
+    select: data => ({
+      pages: data.pages.map(page => ({ data: page })),
+    }),
+  });
+};
+
+export default useMyBookTalkInfiniteScroll;
