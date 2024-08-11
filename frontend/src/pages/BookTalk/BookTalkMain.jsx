ddd762af8d993006039c5b233fdb9a08b30cdffd
김지윤 <yunva17@naver.com>
2024-08-12 03:53:13 +0900

    fix : 참여한 도서 없는 경우 추가

--- frontend/src/pages/BookTalk/BookTalkMain.jsx
+++ frontend/src/pages/BookTalk/BookTalkMain.jsx
@@ -52,18 +52,32 @@ const BookTalkMain = () => {
         <h2 className='text-green-400 text-lg font-bold mb-4'>
           ✅ 내가 참여한 도서
         </h2>
-        {participatedBooks?.slice(0, 3)?.map((book, index) => (
-          <BookTalkItem
-            key={index}
-            book={book}
-            onClick={() => handleDetailPageMove(book)}
-          />
-        ))}
-        <div className='flex justify-end text-sm cursor-pointer'>
-          <button className='text-gray-500' onClick={handleMorePageMove}>
-            더보기
-          </button>
-        </div>
+
+        {participatedBooks?.length > 0 ? (
+          participatedBooks
+            ?.slice(0, 3)
+            .map((book, index) => (
+              <BookTalkItem
+                key={index}
+                book={book}
+                onClick={() => handleDetailPageMove(book)}
+              />
+            ))
+        ) : (
+          <div className='flex flex-col items-center text-center bg-gray-100 space-y-4 p-4 m-4 rounded'>
+            <p>
+              아직 참여한 도서가 없습니다. <br />
+              북톡에 참여해보세요!
+            </p>
+          </div>
+        )}
+        {participatedBooks.length > 0 && (
+          <div className='flex justify-end text-sm cursor-pointer'>
+            <button className='text-gray-500' onClick={handleMorePageMove}>
+              더보기
+            </button>
+          </div>
+        )}
       </div>
       <div className='mt-6'>
         <h2 className='text-pink-500 text-lg font-bold mb-4'>
