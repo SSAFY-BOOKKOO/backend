37c13bbdbe9a5dba5595f7c06e1aafb23e0e9b17
ys4512558 <ys4512558@naver.com>
2024-08-12 16:46:58 +0900

    feat: minIO 설정 및 파일 관련 로직 구현체 추가

---
+++ backend/common-service/src/main/java/com/ssafy/bookkoo/commonservice/file/config/MinioConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.bookkoo.commonservice.file.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Value("${minio.url}")
+    private String url;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                          .endpoint(url)
+                          .credentials(accessKey, secretKey)
+                          .build();
+    }
+}
