64dc157db36fdd0f305c3fcbec724bf04e89984d
ys4512558 <ys4512558@naver.com>
2024-08-13 09:01:39 +0900

    comment: 주석 제거

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/MemberServiceApplication.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/MemberServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
-//시큐리티 자동 설정 전체 비활성화 (GateWay에서 사용하기 때문에)
 @EnableFeignClients
 @SpringBootApplication
 public class MemberServiceApplication {
