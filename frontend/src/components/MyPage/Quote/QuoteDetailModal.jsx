033f5b22b7e6be0fccae1b3c1f7d474d1b1df624
김지윤 <yunva17@naver.com>
2024-08-13 17:52:59 +0900

    feat: 캡쳐 기능, 글귀 배경 변경

--- frontend/src/components/MyPage/Quote/QuoteDetailModal.jsx
+++ frontend/src/components/MyPage/Quote/QuoteDetailModal.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import backgroundImage from '@assets/images/quote_background.png';
 import IconButton from '@components/@common/IconButton';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { IoCloseSharp } from 'react-icons/io5';
 import { BiSolidQuoteAltLeft } from 'react-icons/bi';
+import CaptureButton from '../../Library/Main/CaptureButton';
 
 const QuoteDetailModal = ({
   guoteData,
@@ -11,9 +12,19 @@ const QuoteDetailModal = ({
   onNextQuote,
   onPrevQuote,
 }) => {
+  const ref = useRef(null);
+  const [isCapturing, setIsCapturing] = useState(false);
+
+  const handleCapture = capturing => {
+    setIsCapturing(capturing);
+  };
+
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-20'>
-      <div className='bg-white rounded-lg shadow-lg w-full max-w-3xl relative mx-2 h-3/5'>
+      <div
+        ref={ref}
+        className='bg-white rounded-lg shadow-lg w-11/12 relative mx-2 h-3/5'
+      >
         <img
           src={backgroundImage}
           alt='Background'
@@ -24,27 +35,38 @@ const QuoteDetailModal = ({
           <p className='text-lg font-semibold mb-4'>{guoteData?.content}</p>
           <p className='text-sm'>{guoteData?.source}</p>
         </div>
-        <div className='absolute top-0 right-0 mt-4 mr-4'>
-          <IconButton
-            onClick={() => setSelectedQuote(null)}
-            icon={IoCloseSharp}
-            className='bg-transparent text-black p-2 rounded-full text-2xl'
-          />
-        </div>
-        <div className='absolute bottom-0 right-0 mb-4 mr-4'>
-          <IconButton
-            onClick={onNextQuote}
-            icon={FaAngleRight}
-            className='bg-transparent text-black p-2 rounded-full text-2xl'
-          />
-        </div>
-        <div className='absolute bottom-0 left-0 mb-4 ml-4'>
-          <IconButton
-            onClick={onPrevQuote}
-            icon={FaAngleLeft}
-            className='bg-transparent text-black p-2 rounded-full text-2xl'
-          />
-        </div>
+        {!isCapturing && (
+          <div>
+            <div className='absolute top-0 right-0 mt-4 mr-4 z-10'>
+              <IconButton
+                onClick={() => setSelectedQuote(null)}
+                icon={IoCloseSharp}
+                className='bg-transparent text-black p-2 rounded-full text-2xl'
+              />
+            </div>
+            <div className='absolute bottom-0 right-0 mb-4 mr-4 z-10'>
+              <IconButton
+                onClick={onNextQuote}
+                icon={FaAngleRight}
+                className='bg-transparent text-black p-2 rounded-full text-2xl'
+              />
+            </div>
+            <div className='absolute inset-0 flex justify-center items-end mb-4'>
+              <CaptureButton
+                targetRef={ref}
+                filename='quote'
+                onCapture={handleCapture}
+              />
+            </div>
+            <div className='absolute bottom-0 left-0 mb-4 ml-4 z-10'>
+              <IconButton
+                onClick={onPrevQuote}
+                icon={FaAngleLeft}
+                className='bg-transparent text-black p-2 rounded-full text-2xl'
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
