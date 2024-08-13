04eaf254c359f460d5e4514e97225003201364d2
김지윤 <yunva17@naver.com>
2024-08-13 10:57:15 +0900

    fix: 글귀 페이지 수정

--- frontend/src/components/MyPage/Quote/QuoteList.jsx
+++ frontend/src/components/MyPage/Quote/QuoteList.jsx
@@ -3,15 +3,21 @@ import { BiSolidQuoteAltLeft, BiSolidQuoteAltRight } from 'react-icons/bi';
 import useModal from '@hooks/useModal';
 import SettingsModal from '../../@common/SettingsModal';
 
-const QuoteList = ({ quotes, onQuoteClick }) => {
+const QuoteList = ({ quotes, onQuoteClick, onEditClick, onDeleteClick }) => {
   const [activeIndex, setActiveIndex] = useState(null);
   const { isOpen, toggleModal } = useModal();
 
   // 글귀 수정
-  const handleQuoteUpdate = index => {};
+  const handleQuoteUpdate = index => {
+    const quote = quotes[index];
+    onEditClick(quote); // 수정 input 열기
+  };
 
   // 글귀 삭제
-  const handleQuoteDelete = index => {};
+  const handleQuoteDelete = index => {
+    const quote = quotes[index];
+    onDeleteClick(quote.id); // 글귀 삭제
+  };
 
   const handleSettingsClick = (e, index) => {
     e.stopPropagation();
@@ -26,7 +32,7 @@ const QuoteList = ({ quotes, onQuoteClick }) => {
 
   return (
     <div>
-      {quotes.map((quoteObj, index) => (
+      {quotes.map((quoteData, index) => (
         <div
           key={index}
           className='bg-pink-50 p-6 rounded-lg shadow mb-4 cursor-pointer border border-pink-200 relative'
@@ -43,13 +49,13 @@ const QuoteList = ({ quotes, onQuoteClick }) => {
           </div>
           <div className='flex flex-col justify-center items-center my-2'>
             <p className='text-lg font-medium text-gray-800'>
-              {quoteObj.quote}
+              {quoteData.content}
             </p>
           </div>
           <div className='flex justify-end'>
             <BiSolidQuoteAltRight />
           </div>
-          <p className='mt-4 text-sm text-gray-600'>{quoteObj.info}</p>
+          <p className='mt-4 text-sm text-gray-600'>{quoteData.source}</p>
         </div>
       ))}
     </div>
--- frontend/src/pages/Mypage/Quote.jsx
+++ frontend/src/pages/Mypage/Quote.jsx
@@ -1,21 +1,92 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import QuoteList from '@components/MyPage/Quote/QuoteList.jsx';
 import QuoteInput from '@components/MyPage/Quote/QuoteInput.jsx';
 import QuoteDetailModal from '@components/MyPage/Quote/QuoteDetailModal.jsx';
 import QuoteCreateModal from '@components/MyPage/Quote/QuoteCreateModal.jsx';
-import quotesData from '@mocks/QuoteData';
 import useModal from '@hooks/useModal';
 import Button from '@components/@common/Button';
 import WrapContainer from '@components/Layout/WrapContainer';
+import { useAtom } from 'jotai';
+import { showAlertAtom } from '@atoms/alertAtom';
+import Spinner from '@components/@common/Spinner';
+import useQuoteInfiniteScroll from '@hooks/useQuoteInfiniteScroll';
+import { useInView } from 'react-intersection-observer';
+import {
+  postQuote,
+  deleteQuote,
+  putQuote,
+  getQuoteCount,
+} from '@services/Member';
 
 const Quote = () => {
-  const [quotes, setQuotes] = useState(quotesData);
+  const [quotes, setQuotes] = useState([]);
+  const [quoteCount, setQuoteCount] = useState();
   const [showInput, setShowInput] = useState(false);
   const [selectedQuoteIndex, setSelectedQuoteIndex] = useState(null);
+  const [extractedText, setExtractedText] = useState('');
+  const [editQuote, setEditQuote] = useState(null); // 수정할 quote
   const { isOpen, toggleModal } = useModal();
+  const [, showAlert] = useAtom(showAlertAtom);
 
-  const addQuote = quoteObj => {
-    setQuotes([...quotes, quoteObj]);
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    refetch,
+  } = useQuoteInfiniteScroll();
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
+  useEffect(() => {
+    if (data?.pages) {
+      const fetchedQuotes = data.pages.flatMap(page => page);
+      setQuotes(fetchedQuotes);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    handleQuoteCount();
+  }, []);
+
+  const createQuote = async quoteData => {
+    try {
+      await postQuote(quoteData.content, quoteData.source);
+      refetch();
+      showAlert('글귀가 생성되었습니다.', false, () => {});
+      setShowInput(false);
+    } catch (error) {
+      showAlert('앗 오류가 발생했습니다. 다시 시도해주세요!', true, () => {});
+    }
+  };
+
+  const updateQuote = async quoteData => {
+    try {
+      await putQuote(quoteData.quoteId, quoteData.content, quoteData.source);
+      refetch();
+      showAlert('글귀가 수정되었습니다.', false, () => {});
+      setShowInput(false);
+      setEditQuote(null);
+    } catch (error) {
+      showAlert('앗 오류가 발생했습니다. 다시 시도해주세요!', true, () => {});
+    }
+  };
+
+  const deleteQuoteHandler = async quoteId => {
+    try {
+      await deleteQuote(quoteId);
+      refetch();
+      showAlert('글귀가 삭제되었습니다.', false, () => {});
+    } catch (error) {
+      showAlert('앗 오류가 발생했습니다. 다시 시도해주세요!', true, () => {});
+    }
   };
 
   const handleQuoteClick = index => {
@@ -34,6 +105,25 @@ const Quote = () => {
     );
   };
 
+  const handleImageUpload = text => {
+    setExtractedText(text);
+    setShowInput(true);
+  };
+
+  const handleQuoteCount = async () => {
+    try {
+      const data = await getQuoteCount();
+      setQuoteCount(data);
+    } catch (e) {
+      console.log('error');
+    }
+  };
+
+  const handleEditClick = quote => {
+    setEditQuote(quote);
+    setShowInput(true);
+  };
+
   return (
     <WrapContainer>
       <div className='flex justify-between items-center mb-4'>
@@ -42,20 +132,39 @@ const Quote = () => {
           <Button onClick={toggleModal}>글귀 만들기</Button>
         </div>
       </div>
-      <p className='mb-3'>총 {quotes?.length}개</p>
-      <QuoteList quotes={quotes} onQuoteClick={handleQuoteClick} />
+      <p className='mb-3'>총 {quoteCount}개</p>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <QuoteList
+          quotes={quotes}
+          onQuoteClick={handleQuoteClick}
+          onEditClick={handleEditClick}
+          onDeleteClick={deleteQuoteHandler}
+        />
+      )}
+      <div ref={ref}></div>
+      {isFetchingNextPage && <Spinner infiniteScroll />}
       {isOpen && (
         <QuoteCreateModal
           toggleModal={toggleModal}
           setShowInput={setShowInput}
+          onImageUpload={handleImageUpload}
         />
       )}
       {showInput && (
-        <QuoteInput addQuote={addQuote} setShowInput={setShowInput} />
+        <QuoteInput
+          addQuote={editQuote ? updateQuote : createQuote}
+          setShowInput={setShowInput}
+          initialQuote={editQuote ? editQuote.content : extractedText}
+          initialSource={editQuote ? editQuote.source : ''}
+          isEdit={!!editQuote} // 수정 여부
+          quoteId={editQuote ? editQuote.id : null} // 수정할 때 quoteId 전달
+        />
       )}
       {selectedQuoteIndex !== null && (
         <QuoteDetailModal
-          quoteObj={quotes[selectedQuoteIndex]}
+          guoteData={quotes[selectedQuoteIndex]}
           setSelectedQuote={setSelectedQuoteIndex}
           onNextQuote={handleNextQuote}
           onPrevQuote={handlePrevQuote}
