0e45cccc477b7dda17ae5be73c172ff172599405
ys4512558 <ys4512558@naver.com>
2024-08-13 14:24:20 +0900

    fix: 유효성 정책으로 인한 검증 부분 삭제

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestFollowShipDto.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestFollowShipDto.java
@@ -1,9 +1,6 @@
 package com.ssafy.bookkoo.memberservice.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record RequestFollowShipDto(
-    @NotBlank(message = "팔로워의 ID는 공백일 수 없습니다.")
     String memberId
 ) {
 
