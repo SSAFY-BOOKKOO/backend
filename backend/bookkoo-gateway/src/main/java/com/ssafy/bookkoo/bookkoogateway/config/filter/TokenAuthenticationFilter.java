33f87c77ba835dde60687e7077318152420dd19d
ys4512558 <ys4512558@naver.com>
2024-08-14 12:44:44 +0900

    feat: 에러 발생 시 throw 구문 추가 및 로그 추가

--- backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/filter/TokenAuthenticationFilter.java
+++ backend/bookkoo-gateway/src/main/java/com/ssafy/bookkoo/bookkoogateway/config/filter/TokenAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.ssafy.bookkoo.bookkoogateway.config.filter;
 
 import com.ssafy.bookkoo.bookkoogateway.client.MemberServiceWebClient;
 import com.ssafy.bookkoo.bookkoogateway.exception.AccessTokenExpirationException;
+import com.ssafy.bookkoo.bookkoogateway.exception.MemberNotFoundException;
 import com.ssafy.bookkoo.bookkoogateway.util.TokenUtils;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -116,8 +117,10 @@ public class TokenAuthenticationFilter implements GlobalFilter {
                                                              authentication));
                                      })
                                      .doOnError(error -> {
-                                         log.error("토큰 필터 에러 {}", error.getMessage());
-                                     });
+                                         log.info("[gateway - 멤버 서비스 호출 에러] : {}",
+                                             error.getMessage());
+                                         throw new MemberNotFoundException();
+                                     } );
     }
 
     private static boolean isNotSkip(String excludedPath, String path) {
