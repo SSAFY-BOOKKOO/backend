e01f85c0e6568aab288995ce55d11201f5345533
김인엽 <inyub98@gmail.com>
2024-08-14 09:58:36 +0900

    fix : headers -> memberId 파라미터 받기로 변경

--- backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/controller/ReviewController.java
+++ backend/book-service/src/main/java/com/ssafy/bookkoo/bookservice/controller/ReviewController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -112,10 +113,8 @@ public class ReviewController {
             """
     )
     public ResponseEntity<HttpStatus> deleteReviewOfMember(
-        @RequestHeader HttpHeaders headers
+        @RequestParam("memberId") Long memberId
     ) {
-        Long memberId = CommonUtil.getMemberId(headers);
-
         reviewService.deleteReviewsByMemberId(memberId);
 
         return ResponseEntity.noContent()
