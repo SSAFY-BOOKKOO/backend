c78b4727b5e715bc9162f0e281f93063e283bf8f
ys4512558 <ys4512558@naver.com>
2024-08-14 14:24:46 +0900

    feat: 글귀관련 DTO 폰트 이름, 색상, 사이즈 칼럼 추가

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestCreateQuoteDto.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestCreateQuoteDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.bookkoo.memberservice.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,7 +12,23 @@ public record RequestCreateQuoteDto(
     String source,
     @NotBlank(message = "글귀는 공백일 수 없습니다.")
     @Size(max = 200, message = "글귀는 최대 200자 입니다.")
-    String content
+    String content,
+    @Schema(example = "Pretendard")
+    String fontName,
+    @Schema(example = "text-black")
+    String fontColor,
+    @Schema(example = "2")
+    Integer fontSize
 ) {
-
+    public RequestCreateQuoteDto {
+        if (fontName == null) {
+            fontName = "Pretendard";
+        }
+        if (fontColor == null) {
+            fontColor = "text-black";
+        }
+        if (fontSize == null) {
+            fontSize = 2;
+        }
+    }
 }
--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestUpdateQuoteDto.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/dto/request/RequestUpdateQuoteDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.bookkoo.memberservice.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,7 +14,24 @@ public record RequestUpdateQuoteDto(
     String source,
     @NotBlank(message = "글귀는 공백일 수 없습니다.")
     @Size(max = 200, message = "글귀는 최대 200자 입니다.")
-    String content
+    String content,
+    @Schema(example = "Pretendard")
+    String fontName,
+    @Schema(example = "text-black")
+    String fontColor,
+    @Schema(example = "2")
+    Integer fontSize
 ) {
 
+    public RequestUpdateQuoteDto {
+        if (fontName == null) {
+            fontName = "Pretendard";
+        }
+        if (fontColor == null) {
+            fontColor = "text-black";
+        }
+        if (fontSize == null) {
+                fontSize = 2;
+        }
+    }
 }
