ad61dc227cb14adba9c9b6d786ef7f9863e699ec
왕승철 <ys4512558@naver.com>
2024-08-15 22:39:18 +0900

    feat: 캐싱 적용 및 업데이트 시 캐시 업데이트

--- backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/MemberServiceApplication.java
+++ backend/member-service/src/main/java/com/ssafy/bookkoo/memberservice/MemberServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableCaching
+@EnableCaching
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
